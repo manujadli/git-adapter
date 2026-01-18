559c01e17f3e2ab888d74ac15ca86763901503bb
manujadli <manujadli@gmail.com>
2026-01-18 17:33:15 +0530

    Second Commit

--- GraphAdapterApplication.java
+++ GraphAdapterApplication.java
@@ -2,11 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 public class GraphAdapterApplication {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	public static void main(String[] args) {
+		logger.info("GraphAdapterApplication.main()  {} " , "some logger");
 		SpringApplication.run(GraphAdapterApplication.class, args);
 	}
 
